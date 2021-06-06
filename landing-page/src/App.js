b682cb8d0b75df1a849703995dab623bd9b668f0
martincorredor <dev.martincorredor@gmail.com>
2021-06-06 16:24:24 -0500

    |UPDATE| Add main page with static content

--- landing-page/src/App.js
+++ landing-page/src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
-
+import Header from './components/header';
+import Welcome from './components/Welcome';
+import Announcement from './components/announcement';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      </header>
+      <Header/>
+      <Welcome/>
+      <Announcement/>
     </div>
   );
 }

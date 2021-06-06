7ce7b110f0493b92a0586f4b4ff68ebdd47839cf
martincorredor <dev.martincorredor@gmail.com>
2021-06-06 16:25:54 -0500

    |UPDATED| Add new components: announcement, header and welcome

---
+++ landing-page/src/components/header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <nav className="Header">
+            <ul className="NavBar">
+                <li className="NavBar-Items"><a href="" class="Color-white">SunSquare</a></li>
+                <li className="NavBar-Items"><a href="" class="Color-white">Home</a></li>
+                <li className="NavBar-Items"><a href="" class="Color-white">My announcements</a></li>
+                <li className="NavBar-Items"><a href="" class="Color-white">Create your ad</a></li>
+            </ul>
+            <div>
+                <a href="" class="Color-white">Profile</a>
+            </div>
+        </nav>
+    )
+}
+
+export default Header;

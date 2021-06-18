19fcf951663bcf0ff03ee70d1b254770e74e4bff
martincorredor <dev.martincorredor@gmail.com>
2021-06-18 10:40:04 -0500

    |UPDATE| Fix team image

--- landing-page/src/components/team.js
+++ landing-page/src/components/team.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import fotoJuli from '../img/photos/fotoJuli.jpg'
+import fotoNelson from '../img/photos/fotoNelson.jpg'
+import fotoMartin from '../img/photos/fotoMartin.jpg'
 
 
 const Team = ()=> {
@@ -10,20 +13,20 @@ const Team = ()=> {
             <div className="team">
                 
                 <div className="team-person">
-                    <img className="team-img" src="https://media-exp3.licdn.com/dms/image/C4E03AQFK110Qqk7vYQ/profile-displayphoto-shrink_800_800/0/1605896882257?e=1629331200&v=beta&t=GLSSMvnOGOTcH9PmGO0ZpRfQ6TQMtJEiJg4Z7a5O8kU"/>
+                    <img className="team-img" src={fotoJuli}/>
                     <div className="bold">Juliana Monroy</div>
                     <div className="bold">Back-end Developer</div>
                     <a className="team-linkedin" href="https://www.linkedin.com/in/juliana-monroy-perez">Linkedin</a>
                 </div>
                 
                 <div className="team-person">
-                    <img className="team-img" src="https://media-exp3.licdn.com/dms/image/C4E03AQFxDHkY7rZvJg/profile-displayphoto-shrink_800_800/0/1615440578820?e=1629331200&v=beta&t=E88VgP3miEybKpbLZUhVDvamJjN2rNAfovE2Evab4FQ"/>
+                    <img className="team-img" src={fotoNelson}/>
                     <div className="bold">Nelson Arevalo</div>
                     <div className="bold">Back-end Developer</div>
                     <a className="team-linkedin" href="https://www.linkedin.com/in/near-fuentes/">Linkedin</a>
                 </div>
                 <div className="team-person">
-                    <img className="team-img" src="https://media-exp3.licdn.com/dms/image/C4D03AQEUMMYSSXgvCQ/profile-displayphoto-shrink_800_800/0/1591663856242?e=1629331200&v=beta&t=hxJp6_WfdYfHUroSSr0QVfr-Uo5WBKFjfO4p1LlTnzU"/>
+                    <img className="team-img" src={fotoMartin}/>
                     <div className="bold">Martin Corredor</div>
                     <div className="bold">Front-end Developer</div>
                     <a className="team-linkedin" href="https://www.linkedin.com/in/martin-corredor/">Linkedin</a>

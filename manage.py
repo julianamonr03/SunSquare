e34a50d45c217d428f9cead62e62869fcffa20c9
julianamonr03 <julianamonroy03@hotmail.com>
2021-07-05 20:50:44 -0500

    [UPDATE] - Updating manage file - running app

--- manage.py
+++ manage.py
@@ -1,16 +1,6 @@
-from config import config
+from config import app
 from routes.base_routes import initiate_app
-from flask_script import Manager, Server
-
-
-configuration = config["development"]
-app = initiate_app(configuration)
-
-
-manager = Manager(app)
-# Running app with "python3 manage.py runserver"
-manager.add_command("runserver", Server(host="127.0.0.1", port="5000"))
 
 
 if __name__ == "__main__":
-    manager.run()
+    app.run(debug=True)

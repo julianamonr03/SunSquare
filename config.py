17a3ed2ebbde2fb9d662678143ff67100cfeb95e
julianamonr03 <julianamonroy03@hotmail.com>
2021-07-05 20:50:07 -0500

    [UPDATE] - Updating configuration file

--- config.py
+++ config.py
@@ -3,25 +3,17 @@ from flask_sqlalchemy import SQLAlchemy
 
 """ Configuration file """
 
-# Secret key for password config
-class Config:
-    SECRET_KEY = "secret123345key"
-
 
 app = Flask(__name__)
 
 
+app.secret_key = 'my12345secret0133key'
+
 # sqlalchemy configuration for development
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = 'False'
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqldb://root@localhost/sunsquare'
+
+
 # Save DB SQLAlchemy app
 db = SQLAlchemy(app)
-
-class DevelopmentConfig(Config):
-    DEBUG = True
-    MYSQL_HOST = "localhost"
-    MYSQL_USER = "root"
-    MYSQL_PASSWORD = ""
-    MYSQL_DB = "sunsquare"
-
-config = {"development": DevelopmentConfig, "default": DevelopmentConfig}

7e4d3bcc3b3f2e91710b835d1e19e3f55055429b
julianamonr03 <julianamonroy03@hotmail.com>
2021-07-05 20:52:17 -0500

    [UPDATE] - Base routes for application

--- routes/base_routes.py
+++ routes/base_routes.py
@@ -1,4 +1,21 @@
+from flask import Blueprint, blueprints, render_template
 from config import app
+from flask_login import login_required, current_user
+
+blueprint = Blueprint('blueprint', __name__)
+
+
+@blueprint.route('/')
+def index():
+    """ Route for index page """
+    # return ("ok")
+    return render_template('index.html')
+
+
+@blueprint.route('/profile')
+#@login_required
+def profile():
+    return render_template('profile.html', name='juli')
 
 
 def initiate_app(config):
@@ -7,3 +24,4 @@ def initiate_app(config):
     # app.register_error_handler(404, page_not_found)
     # app.register_error_handler(401, page_not_authorized)
     return app
+
